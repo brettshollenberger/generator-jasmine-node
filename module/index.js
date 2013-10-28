39f5dbd807c5a8b8ec5711279791771d047f923a
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-27 23:34:17 -0400

    Add Module subgenerator

---
+++ module/index.js
@@ -0,0 +1,19 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var ModuleGenerator = module.exports = function ModuleGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+
+  console.log('Creating module ' + this.name + '.');
+};
+
+util.inherits(ModuleGenerator, yeoman.generators.NamedBase);
+
+ModuleGenerator.prototype.files = function files() {
+  this.moduleName = this.name;
+  this.template('../../app/templates/_mod.js', 'lib/' + this.moduleName + '.js');
+  this.template('../../app/templates/_mod.spec.js', 'spec/' + this.moduleName + '.spec.js');
+};
