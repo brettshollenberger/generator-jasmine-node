1cde93ac68ecec82b103c55819c023e5a59e9daa
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-27 23:20:24 -0400

    Add Generator-Jasmine-Node

---
+++ README.md
@@ -0,0 +1,23 @@
+# Generator-Jasmine-Node [![Build Status](https://secure.travis-ci.org/brettshollenberger/generator-jasmine-node.png?branch=master)](https://travis-ci.org/brettshollenberger/generator-jasmine-node)
+
+A generator for [Yeoman](http://yeoman.io).
+
+## Getting Started
+
+To install generator-jasmine-node from npm, run:
+
+```
+$ npm install -g generator-jasmine-node
+```
+
+Finally, initiate the generator:
+
+```
+$ yo jasmine-node
+```
+
+Name your first module, and you're on your way to TDDing dat app.
+
+jasmine-node spec/
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,49 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var JasmineNodeGenerator = module.exports = function JasmineNodeGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(JasmineNodeGenerator, yeoman.generators.Base);
+
+JasmineNodeGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    name: 'moduleName',
+    message: 'What would you like to call your first module?'
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.moduleName = props.moduleName;
+
+    cb();
+  }.bind(this));
+};
+
+JasmineNodeGenerator.prototype.app = function app() {
+  this.mkdir('lib');
+  this.mkdir('spec');
+  this.template('_mod.js', 'lib/' + this.moduleName + '.js');
+  this.template('_mod.spec.js', 'spec/' + this.moduleName + '.spec.js');
+
+  this.copy('_package.json', 'package.json');
+};
+
+JasmineNodeGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
